cc02ddea702da2072aa431299fc99eed9883053c
axelolea <axelcedilloolea@gmail.com>
2023-09-12 02:50:09 -0600

    Create and imp user service

---
+++ src/main/java/com/axelolea/inflapartybackend/dto/UserRegister.java
@@ -0,0 +1,13 @@
+package com.axelolea.inflapartybackend.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserRegister {
+
+    private String email;
+
+    private String password;
+
+}
--- src/main/java/com/axelolea/inflapartybackend/services/UserService.java
+++ src/main/java/com/axelolea/inflapartybackend/services/UserService.java
@@ -1,26 +1,9 @@
 package com.axelolea.inflapartybackend.services;
 
-
-import com.axelolea.inflapartybackend.models.User;
-import com.axelolea.inflapartybackend.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import com.axelolea.inflapartybackend.models.account.User;
 
 import java.util.List;
 
-@Service
-public class UserService {
-
-    private final UserRepository userRepo;
-    @Autowired
-    public UserService(UserRepository userRepo) {
-        this.userRepo = userRepo;
-    }
-
-
-    public List<User> getAllUsers () {
-        return userRepo.findAll();
-    }
-
-
+public interface UserService {
+    List<User> getAllUsers ();
 }
