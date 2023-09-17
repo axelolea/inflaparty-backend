71a9bc016bf3695b5ba11883c29d1b14fd89eb5b
axelolea <axelcedilloolea@gmail.com>
2023-09-17 05:04:07 -0600

    Rename Controler and service

--- src/main/java/com/axelolea/inflapartybackend/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.axelolea.inflapartybackend.services;
-
-import com.axelolea.inflapartybackend.models.account.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> getAllUsers ();
-}
