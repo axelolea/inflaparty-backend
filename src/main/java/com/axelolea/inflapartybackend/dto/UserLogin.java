7013241a3cb7039ab9631ce0afe1ba54e1795841
axelolea <axelcedilloolea@gmail.com>
2023-09-18 22:31:49 -0600

    Update models login and register

---
+++ src/main/java/com/axelolea/inflapartybackend/dto/UserLogin.java
@@ -0,0 +1,12 @@
+package com.axelolea.inflapartybackend.dto;
+
+import lombok.Data;
+
+
+@Data
+public class UserLogin {
+
+    private String email;
+    private String password;
+
+}
--- src/main/java/com/axelolea/inflapartybackend/dto/UserRegister.java
+++ src/main/java/com/axelolea/inflapartybackend/dto/UserRegister.java
@@ -3,11 +3,16 @@ package com.axelolea.inflapartybackend.dto;
 
 import lombok.Data;
 
+
 @Data
 public class UserRegister {
 
     private String email;
-
     private String password;
+    private String password2;
+    private String name;
+    private String lastname;
+    private String phone;
+    private String photoUrl;
 
 }
